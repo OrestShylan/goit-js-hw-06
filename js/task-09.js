2911508e200b5029f103d625060035f3cf823f03
OrestShylan <orestshylan1997@gmail.com>
2023-01-14 16:08:04 +0200

    update

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.querySelector('body')
+const span = document.querySelector('.color')
+const button = document.querySelector('.change-color')
+
+button.addEventListener('click', onBodyColorChange)
+
+function onBodyColorChange(event) {
+const randomHexColor = getRandomHexColor();
+
+  body.style.backgroundColor = randomHexColor;
+  span.textContent = randomHexColor;
+}
