9f6615d629092ecdc08f2ceb723d1889166582a4
OrestShylan <orestshylan1997@gmail.com>
2023-01-14 15:56:48 +0200

    update

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const ulEl = document.querySelector('#ingredients');
+const liEl = document.createElement('li');
+
+for (const ingridient of ingredients){
+  const liEl = document.createElement('li');
+  liEl.textContent = ingridient;
+  ulEl.appendChild(liEl); 
+}
